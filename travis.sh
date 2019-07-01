2ae10b7af0778c407830eb715705e6ae1928c126
Sebastian Hollermann <sebastian.hollermann@stud.hs-ruhrwest.de>
2019-07-01 15:07:31 +0200

    Set Variables IS_REALESE and IS_RC

--- travis.sh
+++ travis.sh
@@ -188,18 +188,16 @@ checkVersion() {
   export PROJECT_VERSION=$(mvn help:evaluate -N -Dexpression=project.version|grep -v '\[')
   echo "Found Version ${PROJECT_VERSION} in pom.xml"
 
+  export IS_RC=""
+  export IS_RELEASE=""
   if [ ${TRAVIS_TAG} ]; then
     # Check if it is a Release
     if matchRegex ${TRAVIS_TAG} ${RELEASE_VERSION_REGEX}; then
       export IS_RELEASE="1"
-    else 
-      export IS_RELEASE=""
     fi
     # Check for Release Candidate
     if matchRegex ${TRAVIS_TAG} ${RC_VERSION_REGEX}; then 
       export IS_RC="1"
-    else 
-      export IS_RC=""
     fi
   fi
   # Check for Snapshot
